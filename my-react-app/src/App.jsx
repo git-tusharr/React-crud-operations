306920925b4e007fbabf73128f9129a2d8ff23c7
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-07-17 04:19:57 +0530

    JSX

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,17 @@
-import Cybrom from "./Cybrom";
 
 const App=()=>{
   return(
-    <Cybrom/>
+
+    <>
+    <h1>my Name is Tushar </h1>
+    <hr />
+    name : <input type="text" name="" id="" />
+    <hr />
+    age : <input type="text" name="" id="" />
+    <hr />
+    <button>Submit</button>
+    </>
   )
 }
+
 export default App;

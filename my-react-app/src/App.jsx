3c24c165edd1eed83640226fa865a51fdf641733
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-07-22 21:41:06 +0530

    css in react

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,17 +1,29 @@
-import Students from "./Students";
-const name="tushar";
-const rollno=342;
-const stuclass="btech";
-const totalfees=90000;
 
 const App=()=>{
-  return(
+  
 
+ 
+
+  
+  return(
     <>
-    <h1>Welcome </h1>
-    <Students name={name} rno={rollno} class={stuclass} fees={totalfees} /> 
+
+    <div className="data">
+      <h2>Cybrom</h2>
+    </div>
+
+    <div className="lorem">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla neque, temporibus porro illo totam harum veniam non labore ducimus nemo voluptates fugit hic in tenetur molestias ipsa deleniti id omnis?
+    </div>
+
+    <div className="">
+      <h1>Our Company</h1>
+    </div>
+    
+
+    
     </>
   )
 }
 
-export default App; 
+export default App;

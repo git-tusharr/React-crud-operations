e9dce5873452773dab26f091b3854b0dce7a46d8
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-07-26 22:35:12 +0530

    useState And useEffect

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,84 +1,22 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
-import Carousel from 'react-bootstrap/Carousel';
-
-import img1 from "./images/b1.jpg"
-import img2 from "./images/b2.jpg"
-import img3 from "./images/b3.jpg"
-
-
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
-
+import { useState,useEffect } from "react";
 const App=()=>{
+  const [count,setCount]=useState(0);
+  const [multi,setMulti]=useState(0);
+
+  useEffect(()=>{
+    setMulti(count*2)
+  },[count])
+
+
+return(
+  <>
   
+  <h1>My count : {count}</h1>
+  <h1>My multiplication : {multi}</h1>
 
- 
-
+  <button onClick={()=>{setCount(count+1)}}>Click here!</button>
   
-  return(
-    <>
-    <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-   
-
-
-    <Carousel>
-      <Carousel.Item>
-        <img src={img1} width="100%" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-         <img src={img2} width="100%"/>
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-         <img src={img3} width="100%" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-      
-
-
-
-       <Card style={{ width: '18rem' }}>
-      <Card.Img  src={img1}/>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    
-
-    
-    </>
-  )
+  </>
+)
 }
-
 export default App;

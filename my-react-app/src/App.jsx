bb9680742aff0ee35de27b7cabfae8278d46042f
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-07-29 23:28:37 +0530

    application form

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,23 +1,44 @@
-import { useState,useEffect } from "react";
+import { useState } from "react";
+
 const App=()=>{
-  const [count,setCount]=useState(0);
-  const [multi,setMulti]=useState(0);
 
-  useEffect(()=>{
-    setMulti(count*2)
-  },[count])
+  const [input,setInput]=useState({});
+  const handleInput=(e)=>{
 
+    let name=e.target.name;
+    let value=e.target.value;
+
+    setInput(values=>({...values,[name]:value}))
+    console.log(input)
+  }
 
 return(
   <>
   
-  <h1>My count : {count}</h1>
-  <h1>My multiplication : {multi}</h1>
+  <h1>Application form</h1>
 
-  <button onClick={()=>{setCount(count+1)}}>Click here!</button>
+  Enter Employ no. : <input type="text" name="empno" onChange={handleInput}/>
+
+  <br />
+
+    Enter Name : <input type="text"  name="name" onChange={handleInput}/>
+
+  <br />
+
+    Enter City : <input type="text"  name="city" onChange={handleInput}/>
+
+
+<br />
+    Enter Salary : <input type="text" name="salary" onChange={handleInput}/>
+
+<br />
+<button>Save</button>
+
   
+
   </>
 )
+
 }
 export default App;
 

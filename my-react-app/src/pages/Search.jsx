119813ddaa7a9b4e234fce840897cbae35f743c7
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-08-06 20:46:12 +0530

    Search functionality added

--- my-react-app/src/pages/Search.jsx
+++ my-react-app/src/pages/Search.jsx
@@ -1,11 +1,57 @@
+import axios from "axios";
+import { useState } from "react";
+
+
 const Search=()=>{
+
+
+    const [rno,setRno]=useState([]);
+    const [mydata,setMydata]=useState([]);
+
+    const handleSubmit=async()=>{
+        let api =`http://localhost:3000/students/?rollno=${rno}`;
+        const response=await axios(api);
+        console.log(response.data);
+        setMydata(response.data)
+    }
+    const ans =mydata.map((key)=>{
+
+        return(
+            <>
+            <tr>
+            <td>{key.rollno}</td>
+            <td>{key.name}</td>
+            <td>{key.city}</td>
+            <td>{key.fees}</td>
+            </tr>
+
+            </>
+        )
+
+    })
+
     return(
         <>
-        
-        <h1>This is my Search pages</h1>
+
+        enter rollno : <input type="text" value={rno} onChange={(e)=>{setRno(e.target.value)}} />
+        <button onClick={handleSubmit}>search</button>
+
+
+        <table border="1" cellPadding={10} align="center">
+            <tr>
+                <th>Rollno</th>
+                <th>Name</th>
+                <th>City</th>
+                <th>Fees</th>
+            </tr>
+            {ans}
+        </table>
+
         
         </>
     )
+
+    
 }
 
 export default Search;
